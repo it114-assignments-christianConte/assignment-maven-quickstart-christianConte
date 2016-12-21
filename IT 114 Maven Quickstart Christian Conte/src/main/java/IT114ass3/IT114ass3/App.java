5ff1b204b4a2c2eaf0fb69561127ea4459b0e579
cmc44 <cmc44@njit.edu>
2016-12-21 01:12:17 -0500

    Add files via upload

---
+++ IT 114 Maven Quickstart Christian Conte/src/main/java/IT114ass3/IT114ass3/App.java
@@ -0,0 +1,13 @@
+package IT114ass3.IT114ass3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
